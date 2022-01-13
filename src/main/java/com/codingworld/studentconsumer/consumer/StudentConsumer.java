29324cbc17fbeb1bafb1f63028bbb9c7c2df1827
Dadasaheb <dadasaheb.gopinath@healthfidelity.com>
2022-01-13 20:53:44 +0530

    Initial commit

---
+++ src/main/java/com/codingworld/studentconsumer/consumer/StudentConsumer.java
@@ -0,0 +1,41 @@
+package com.codingworld.studentconsumer.consumer;
+
+import com.codingworld.studentconsumer.Student;
+import java.lang.reflect.Type;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class StudentConsumer
+{
+
+  @PostConstruct
+  public void consumer()
+  {
+    String url = "http://producer:8012/getStudent";
+    RestTemplate template = new RestTemplate();
+    ResponseEntity<List<Student>> students =template.exchange(url, HttpMethod.GET, HttpEntity.EMPTY,
+        new ParameterizedTypeReference<List<Student>>()
+        {
+          @Override
+          public Type getType()
+          {
+            return super.getType();
+          }
+        });
+    List<Student> list=students.getBody();
+    System.out.println("Size:"+list.size());
+    if(list.size()>0) {
+      for(Student st:list) {
+        System.out.println("id:"+st.getId()+"name:"+st.getName());
+      }
+    }
+  }
+
+}
